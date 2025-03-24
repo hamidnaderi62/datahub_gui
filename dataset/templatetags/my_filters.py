0fc77be13e012a51fd5835b4ad8811535dd17682
حمید نادری <Naderi@khrw.ir>
2025-03-24 18:03:16 +0330

    update persian date

--- dataset/templatetags/my_filters.py
+++ dataset/templatetags/my_filters.py
@@ -12,3 +12,23 @@ def split_by(value, delimiter):
     if not value or not delimiter:
         return []
     return value.split(delimiter)
+
+
+from persiantools.jdatetime import JalaliDate
+from django.utils.timezone import now
+@register.filter
+def to_jalali(value):
+    return JalaliDate(value, locale="fa")
+
+@register.filter
+def to_jalali_s(value):
+    return JalaliDate(value, locale="fa").strftime("%Y/%m/%d")
+@register.filter
+def to_jalali_c(value):
+    return JalaliDate(value, locale="fa").strftime('%c')
+
+
+@register.filter
+def days_ago(value):
+    days = (now().date() - value.date()).days
+    return  (f'{days} روز قبل ')
